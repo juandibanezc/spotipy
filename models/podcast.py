52bee1afc90da7db37d592178dd8abb955d651ec
Juan Ibanez <juan.ibanez@datavalue.ai>
2023-03-08 21:36:30 -0500

    Added show method to podcast

--- models/podcast.py
+++ models/podcast.py
@@ -1,13 +1,24 @@
-from episode import Episode
+from .files import PNGFile
 
 class Podcast:
-    def __init__(self, description, name, image, episodes, tags):
+    def __init__(self, description: str, name: str, duration_ms: int, image_file_path: str, episodes: list, publisher: str):
         self.description = description
         self.name = name
-        self.image = image
+        self.duration_ms = duration_ms
+        self.image = PNGFile(image_file_path)
         self.episodes = episodes
-        self.tags = tags
+        self.publisher = publisher
 
+    def show(self):
+        print('*** Podcast ***')
+        print('* Name:', self.name)
+        print('* Description:', self.description)
+        print('* Total duration:', self.duration_ms)
+        print('* Publisher:', self.publisher)
+        j = 1
+        for i in self.episodes:
+            print(f'{j}. {i.name}')
+            j = j+1
 
     def follow(self):
         pass
